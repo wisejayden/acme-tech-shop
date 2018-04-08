dd9df8f53573705909b02ae9ac373e45e038d8a9
Jayden Wise <jayden.wise@hotmail.com>
2018-04-08 22:47:37 +0200

    Updated path settings

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const compression = require('compression');
+const path = require('path');
 
 app.use(compression());
 app.use(express.static(__dirname + '/public'));
@@ -17,8 +18,10 @@ if (process.env.NODE_ENV != 'production') {
     app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
 
 app.listen(process.env.PORT || 8080, function() {
