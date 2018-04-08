a3cf3946e68b94e70e8ee36f8740dffc67a6e0d9
Jayden Wise <jayden.wise@hotmail.com>
2018-04-08 23:12:33 +0200

    Test

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
 
-// if (process.env.NODE_ENV != 'production') {
-//     app.use(
-//         '/bundle.js',
-//         require('http-proxy-middleware')({
-//             target: 'http://localhost:8081/'
-//         })
-//     );
-// } else {
-//     app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
-// }
-app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+if (process.env.NODE_ENV != 'production') {
+    app.use(
+        '/bundle.js',
+        require('http-proxy-middleware')({
+            target: 'http://localhost:8081/'
+        })
+    );
+} else {
+    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+}
 
 app.get('*', function(req, res) {
     res.sendFile(__dirname + '/index.html');
